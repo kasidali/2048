e8e10714ff57c3eaa1b29f925e89d58903c3ab1e
kasidali <804803082@qq.com>
2017-08-05 19:18:54 +0800

    改

--- main2048.js
+++ main2048.js
@@ -1,19 +1,24 @@
-var board = new Array();//存放数据
+
+var board = new Array();
 var score = 0;
 
 $(document).ready(function(){
     newgame();
 });
 
-function newgame() {
+function newgame(){
+    //初始化棋盘格
     init();
+    //在随机两个格子生成数字
 }
 
-function init() {
-    for(var i = 0; i<4; i++)
-        for(var j = 0; j<4; j++){
-            var gridCell = $("#grid-cell-"+i+"-"+j);
-            gridCell.css('top',  getPosTop(i,j));
-            gridCell.css('left', getPosLeft(i,j));
+function init(){
+    for( var i = 0 ; i < 4 ; i ++ )
+        for( var j = 0 ; j < 4 ; j ++ ){
+
+            var gridCell = $('#grid-cell-'+i+"-"+j);
+            gridCell.css('top', getPosTop( i , j ) );
+            gridCell.css('left', getPosLeft( i , j ) );
         }
+
 }
